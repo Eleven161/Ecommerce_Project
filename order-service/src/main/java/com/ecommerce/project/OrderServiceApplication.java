2a89934b8030bfa11773b21eadad12a1b100cf5b
Upasna <272709@ODCDPW.IC>
2023-02-02 11:30:39 +0530

    swagger implementation

--- order-service/src/main/java/com/ecommerce/project/OrderServiceApplication.java
+++ order-service/src/main/java/com/ecommerce/project/OrderServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -15,6 +16,12 @@ public class OrderServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
+	
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
+	
 	@Bean
 	@LoadBalanced
    public RestTemplate restTemplate() {
--- order-service/src/main/java/com/ecommerce/project/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/ecommerce/project/service/impl/OrderServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ecommerce.project.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.ecommerce.project.common.Payment;
 import com.ecommerce.project.common.TransactionRequest;
@@ -16,19 +18,27 @@ public class OrderServiceImpl implements OrderService {
 	
 	@Autowired
 	private OrderRepo orderRepo;
+	
+	
 	@Autowired
 	private RestTemplate template;
-
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 	@Override
 	public TransactionResponse saveOrder(TransactionRequest request) {
+		
 		String message="";
 		Payment payment=request.getPayment();
 		Order order=request.getOrder();
 		payment.setOrderId(order.getId());
 		payment.setAmount(order.getPrice());
-		Payment paymentResponse=template.postForObject("http://PAYMENT-SERVICE/payment", payment, Payment.class);
+		//Payment paymentResponse=template.postForObject("http://PAYMENT-SERVICE/payment", payment, Payment.class);
+		Payment paymentResponse=webClientBuilder.build()
+		.post()
+		.uri("http://PAYMENT-SERVICE/payment").accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(Payment.class).block();
+		
 		message=paymentResponse.getPaymentStatus().equals("success") 
-				? "payment done and order successfully" : "payment failed"; 
+			? "payment done and order successfully" : "payment failed"; 
 		orderRepo.save(order);
 		return new TransactionResponse(order,paymentResponse.getTransactionId(),paymentResponse.getAmount(),message);
 	}
